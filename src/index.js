2a67b7b1e41ba5f35cfa493e0b5fe4e3ada927b4
ArtemRomanovsky <cheferin1@gmail.com>
2021-08-17 20:42:45 +0300

    add working solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let stringN = n.toString();
+    const arrayN = Array.from(stringN);
+    const resultArray = [];
+    let result = 0;
+    for (let i = arrayN.length - 1; i >= 0; i--) {
+      resultArray.push(arrayN[i]);
+    }
+    stringN = resultArray.join('');
+    result = parseInt(stringN, 10);
+    return result;
+  }
